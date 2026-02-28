5a81ec4475bad2e24d0e2e083973ce703e281b77
RaniaKhan944 <rgulkhan9@gmail.com>
2026-02-28 15:57:47 +0500

    added week3, sorted week2 into a folder

---
+++ week3/h13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+main(){
+int total;
+total = 0;
+
+int num;
+cout<<"ENTER 5 NUMBERS: ";
+cin>>num;
+total = total + num;
+cin>>num;
+total = total + num;
+cin>>num;
+total = total + num;
+cin>>num;
+total = total + num;
+cin>>num;
+total = total + num;
+
+cout<<"SUM IS: "<<total;
+
+
+
+}
